29e93c08b6b670d7a5d8cf50a4383239d248c9ed
jaidenschembri <jaidenschembri1@gmail.com>
2025-06-04 13:48:57 -0700

    hosting fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   root: '.', // ensures Vite uses project-root as root
-  base: '/chartreader/', // GitHub Pages base path
 })
