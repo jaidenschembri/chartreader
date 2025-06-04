4a346e61c3130f70b298f1632ea7e8c88ca35afc
jaidenschembri <jaidenschembri1@gmail.com>
2025-06-04 14:02:54 -0700

    deployment fix #4

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import App from './App.svelte';
 import './style.css';
 
 // Mount the app
-import { mount } from 'svelte';
+import { mount } from 'https://cdn.jsdelivr.net/npm/svelte@5.0.0-next.1/+esm';
 
 mount(App, {
   target: document.getElementById('app'),
