e511f2a30f8157c720fe833baea7843cdd9f113d
jaidenschembri <jaidenschembri1@gmail.com>
2025-06-04 13:44:48 -0700

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   root: '.', // ensures Vite uses project-root as root
+  base: '/chartreader/', // GitHub Pages base path
 })
