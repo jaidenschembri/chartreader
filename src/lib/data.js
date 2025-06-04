6a8503439aaeffb5039c0550a9f835a7d7f2c262
jaidenschembri <jaidenschembri1@gmail.com>
2025-06-04 13:52:12 -0700

    deployment fix

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,9 +1,9 @@
 export async function loadData() {
     const [chineseZodiac, westernZodiac, zodiacVibes, planetaryPositions] = await Promise.all([
-      fetch('/chineseNewYear.json').then(res => res.json()),
-      fetch('/westernZodiac.json').then(res => res.json()),
-      fetch('/zodiacVibes.json').then(res => res.json()),
-      fetch('/planetaryPositions.json').then(res => res.json())
+      fetch('/chartreader/chineseNewYear.json').then(res => res.json()),
+      fetch('/chartreader/westernZodiac.json').then(res => res.json()),
+      fetch('/chartreader/zodiacVibes.json').then(res => res.json()),
+      fetch('/chartreader/planetaryPositions.json').then(res => res.json())
     ]);
     return { chineseZodiac, westernZodiac, zodiacVibes, planetaryPositions };
   }
